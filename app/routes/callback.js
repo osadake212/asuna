2d395831a9828e897179af87d9da89ad518c211e
Osada Takuya <osadake.212.dev@gmail.com>
2016-05-05 02:01:17 +0900

    fix accessor of body json of received message

--- app/routes/callback.js
+++ app/routes/callback.js
@@ -1,12 +1,11 @@
 const sendMessageUrl = "https://trialbot-api.line.me/v1/events";
+const lineHeaders = {
+  'X-Line-ChannelID': process.env.LINE_CHANNEL_ID,
+  'X-Line-ChannelSecret': process.env.LINE_CHANNEL_SECRET,
+  'X-Line-Trusted-User-With-ACL': process.env.LINE_TRUSTED_USER_WITH_ACL
+};
 
-var
-  request = require('request'),
-  lineHeaders = {
-    'X-Line-ChannelID': process.env.LINE_CHANNEL_ID,
-    'X-Line-ChannelSecret': process.env.LINE_CHANNEL_SECRET,
-    'X-Line-Trusted-User-With-ACL': process.env.LINE_TRUSTED_USER_WITH_ACL
-  };
+var request = require('request');
 
 var postMessage = function(mids, message) {
   var options = {
@@ -32,6 +31,17 @@ var postMessage = function(mids, message) {
 };
 
 exports.callback = function(req, res) {
-  postMessage([req.body.content.from], "あすなだよ");
+  console.log(req.body);
+  var
+    ids = [],
+    result = req.body.result;
+
+  for (var key in result) {
+    if (result.hasOwnProperty(key)) {
+      ids.push(result[key].content.from);
+    }
+  }
+
+  postMessage(ids, "あすなだよ");
   res.status(200).send();
 };
