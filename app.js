0fc47007f287ac7b012a691e816c9848463f7b6a
Osada Takuya <osadake.212.dev@gmail.com>
2016-05-04 15:59:03 +0900

    create error handling

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-var express = require('express');
-var app = express();
+var
+  express = require('express'),
+  app = express();
 
 app.get('/', function(req, res) {
   res.send('Hello world!!');
-})
+});
+
+// errro handling
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 app.listen(3000);
